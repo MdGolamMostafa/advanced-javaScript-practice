7e6b789c4a6cae81cf2492b32dea75bc4d46f91e
MdGolamMostafa <shakilhero11@gmail.com>
2020-08-07 16:20:02 +0600

    null vs undefined value

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+let test;
+console.log(test);
+
+function add(num1,num2) {
+    console.log(num1 + num2);
+    return
+}
+const result = add(32,4);
+console.log(result);
+
+
+function add(num1,num2) {
+    console.log(num1);      
+}
+const result = add(3);
+console.log(result);
+
+const person = {name: 'smart muslim',phone:354356};
+console.log(person.salary);
+
+let fun = undefined;
+console.log(fun);
