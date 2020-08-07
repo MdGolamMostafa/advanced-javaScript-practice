5261820c297344cf939b7f4fd7d3bc348604568d
MdGolamMostafa <shakilhero11@gmail.com>
2020-08-07 15:22:57 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//Falsy:
+//0
+//""
+//undefined
+//null
+//NaN
+
+//Truthy
+//'0' , ' '
+//{}
+//[]
+
+
+let name = 0;
+console.log(name);
+if (name || name == 0) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
