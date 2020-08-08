c902464b111efb5e6f818c23e00c6ca2dd5f4942
MdGolamMostafa <shakilhero11@gmail.com>
2020-08-08 11:10:00 +0600

    break , continue

---
+++ breck-continue.js
@@ -0,0 +1,28 @@
+//normal for loop
+
+// const nums = [0,1,2,3,4,5,6,7,8,9];
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+//     console.log(element);
+// }
+
+
+// const nums = [0,1,2,3,4,5,6,7,8,9];
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+//     if (nums[i] > 5) {
+//         //"break" used for stop looping
+//         break;
+//     }
+//     console.log(element);
+// }
+
+const nums = [0,-1,2,3,-4,5,-6,7,-8,9];
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    if (nums[i] < 0) {
+        //"break" used for stop looping
+       continue;
+    }
+    console.log(element);
+}
